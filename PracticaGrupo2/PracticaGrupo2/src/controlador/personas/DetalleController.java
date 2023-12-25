b726978f2a0fde016b3d2d0223b57abdb9441c8a
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2023-12-25 11:21:25 -0500

    Implementacion de los metodos de Ordenamiento en DetalleController y Compare en Detalle Censo

--- PracticaGrupo2/PracticaGrupo2/src/controlador/personas/DetalleController.java
+++ PracticaGrupo2/PracticaGrupo2/src/controlador/personas/DetalleController.java
@@ -1,9 +1,14 @@
 
 package controlador.personas;
 
+import controlador.Excepcion.VacioExcepcion;
 import controlador.TDA.listas.LinkedList;
 import controlador.dao.DataAccessObject;
+import controlador.util.Utilidades;
+import java.lang.reflect.Field;
+import java.util.EmptyStackException;
 import modelo.DetalleCenso;
+import modelo.Persona;
 
 public class DetalleController<T> extends DataAccessObject<DetalleCenso> {
     
@@ -60,11 +65,186 @@ public class DetalleController<T> extends DataAccessObject<DetalleCenso> {
         this.index = index;
     }
     
+      //ORDENAMIENTO SELECCION
+    public LinkedList<DetalleCenso> ordenarSeleccion(Integer type, String field, LinkedList<DetalleCenso> lista) throws EmptyStackException, Exception {
+
+        Integer n = lista.getSize();//Tamaño de la lista
+        DetalleCenso[] m = lista.toArray();//Transformo la lista a arreglo
+        Field faux = Utilidades.getField(DetalleCenso.class, field);
+
+        if (faux != null) {
+            int iteraciones = 0; // Contador de iteraciones
+            for (int i = 0; i < n - 1; i++) {
+                int k = i;
+                DetalleCenso t = m[i];
+                for (int j = i + 1; j < n; j++) {
+                    DetalleCenso mj = m[j];
+                    if (mj.compareTo(t, field, type)) {
+                        t = mj;
+                        k = j;
+                    }
+                    iteraciones++;
+                }
+                m[k] = m[i];
+                m[i] = t;
+            }
+            lista = lista.toList(m);
+            System.out.println("Iteraciones realizadas: " + iteraciones);
+        } else {
+            throw new Exception("No existe ese criterio de busqueda");
+        }
+        return lista;
+    }
+
+    //ORDENAMIENTO MergeSort
+    public LinkedList<DetalleCenso> ordenarMergeSort(LinkedList<DetalleCenso> lista, Integer type, String field) {
+        int[] contador = {0}; // Contador de iteraciones
+        mergeSort(lista, 0, lista.getSize() - 1, type, field, contador);
+        System.out.println("Iteraciones totales realizadas: " + contador[0]);
+
+        return lista;
+    }
+
+    public void mergeSort(LinkedList<DetalleCenso> lista, int ini, int fin, Integer type, String field, int[] contador) {
+        int m = 0;
+        if (ini < fin) {
+            m = (ini + fin) / 2;
+            mergeSort(lista, ini, m, type, field, contador);
+            mergeSort(lista, m + 1, fin, type, field, contador);
+            merge(lista, ini, m, fin, type, field, contador);
+        }
+    }
+
+    public void merge(LinkedList<DetalleCenso> lista, int ini, int m, int fin, Integer type, String field, int[] contador) {
+        DetalleCenso[] array = lista.toArray(); // Transformo la lista a arreglo
+        Field faux = Utilidades.getField(DetalleCenso.class, field);
+
+        int k = 0;
+        int i = ini;
+        int j = m + 1;
+        int n = fin - ini + 1;
+        DetalleCenso b[] = new DetalleCenso[n];
+
+        if (faux != null) {
+            while (i <= m && j <= fin) {
+                if (array[i].compareTo(array[j], field, type)) {
+                    b[k] = array[i];
+                    i++;
+                    k++;
+                } else {
+                    b[k] = array[j];
+                    j++;
+                    k++;
+                }
+                contador[0]++;
+            }
+
+            while (i <= m) {
+                b[k] = array[i];
+                i++;
+                k++;
+                contador[0]++;
+            }
+
+            while (j <= fin) {
+                b[k] = array[j];
+                j++;
+                k++;
+                contador[0]++;
+            }
+
+            for (k = 0; k < n; k++) {
+                array[ini + k] = b[k];
+            }
+            lista = lista.toList(array);
+        }
+
+    }
+
+    //ORDENAMIENTO QUICKSORT
+    public LinkedList<DetalleCenso> ordenarQuickSort(LinkedList<DetalleCenso> lista, Integer type, String field) throws VacioExcepcion {
+        int[] contador = {0};
+        lista = quickSort(lista, 0, lista.getSize() - 1, type, field, contador);
+        System.out.println("Iteraciones totales realizadas: " + contador[0]);
+        return lista;
+    }
+
+    public LinkedList<DetalleCenso> quickSort(LinkedList<DetalleCenso> lista, int izq, int der, Integer type, String field, int[] contador) throws VacioExcepcion {
+        DetalleCenso[] m = lista.toArray(); // Transformo la lista a arreglo
+        Field faux = Utilidades.getField(DetalleCenso.class, field);
+
+        if (faux != null && izq < der) {
+            int i = izq;
+            int j = der;
+
+            // Obtén el pivote
+            DetalleCenso pivote = m[(izq + der) / 2];
+
+            // Realiza el particionamiento
+            while (i <= j) {
+                while (m[i].compareQuickSort(pivote, type, field) < 0) {
+                    i++;
+                    contador[0]++;
+                }
+
+                while (m[j].compareQuickSort(pivote, type, field) > 0) {
+                    j--;
+                    contador[0]++;
+                }
+
+                if (i <= j) {
+                    // Intercambia los elementos
+                    DetalleCenso temp = m[i];
+                    m[i] = m[j];
+                    m[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            lista = lista.toList(m);
+            quickSort(lista, izq, j, type, field, contador); // Ordena la parte izquierda
+            quickSort(lista, i, der, type, field, contador); // Ordena la parte derecha
+        }
+        return lista;
+    }
+   
+    public static void mostrarArreglo(int arreglo[]){
+       
+       for(int i=0; i < arreglo.length; i++){
+           System.out.print(arreglo[i] + "_");
+       }
+       System.out.println("");
+    }
+   
+    public static void mostrarLista(Persona persona[]){
+       for(Persona a: persona){
+           System.out.println(a);
+       }
+    }
+   
     //Compruebo Funciomamiento de la Lista
     public static void main(String[] args){
         DetalleController dtc = new DetalleController();
-        
-        System.out.println(dtc.getDetalles().getSize());
+        try {
+           //ORDENAR SELECCION
+            System.out.println("Probando Ordenamiento SELECCION");
+            System.out.println(dtc.ordenarSeleccion(0, "fechaDivorcio", dtc.getDetalles()).imprimir());
+
+            //LISTA ANTES DE ORDENAR
+            System.out.println("Lista antes de ordenar:");
+            System.out.println(dtc.getDetalles().imprimir());
+            
+            //ORDENAR MERGESORT
+            System.out.println("Ordenar Merge Sort");
+            System.out.println(dtc.ordenarMergeSort(dtc.getDetalles(), 0, "fechaDivorcio").imprimir());
+            
+            //ORDENARQUICKSORT
+            System.out.println("Ordenamieto por QuickSort");
+            System.out.println(dtc.ordenarQuickSort(dtc.getDetalles(), 0, "fechaDivorcio").imprimir());
+            
+           
+        } catch (Exception e) {
+        }
     }
     
 }
--- PracticaGrupo2/PracticaGrupo2/src/modelo/DetalleCenso.java
+++ PracticaGrupo2/PracticaGrupo2/src/modelo/DetalleCenso.java
@@ -1,6 +1,8 @@
 
 package modelo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -89,8 +91,154 @@ public class DetalleCenso {
         this.estadoCivilActual = estadoCivilActual;
     }
 
+   //Metodo Comparar
+    public boolean compareTo(DetalleCenso c, String field, Integer type) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        switch (type) {
+            
+            case 1:  
+                if (field.equalsIgnoreCase("fechaCenso")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaCenso());
+                        Date otherDate = sdf.parse(c.getFechaCenso());
+                        return thisDate.compareTo(otherDate) > 0;
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }else if (field.equalsIgnoreCase("id")) {
+                    return getId().intValue() > c.getId().intValue();
+                
+                }else if (field.equalsIgnoreCase("id_persona")) {
+                    return getId_persona().compareTo(c.getId_persona()) > 0;
+                
+                }else if (field.equalsIgnoreCase("motivo")) {
+                    return getMotivo().compareTo(c.getMotivo()) > 0;
+                
+                }else if (field.equalsIgnoreCase("fechaDivorcio")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaDivorcio());
+                        Date otherDate = sdf.parse(c.getFechaDivorcio());
+                        return thisDate.compareTo(otherDate) > 0;
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }else if (field.equalsIgnoreCase("estadoCivilAnterior")) {
+                    return getEstadoCivilAnterior().compareTo(c.getEstadoCivilAnterior()) > 0;
+                
+                }else if (field.equalsIgnoreCase("estadoCivilActual")) {
+                    return getEstadoCivilActual().compareTo(c.getEstadoCivilActual()) > 0;
+                
+                }
+            case 0:
+                 if (field.equalsIgnoreCase("fechaCenso")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaCenso());
+                        Date otherDate = sdf.parse(c.getFechaCenso());
+                        return thisDate.compareTo(otherDate) < 0;
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }else if (field.equalsIgnoreCase("id")) {
+                    return getId().intValue() < c.getId().intValue();
+                
+                }else if (field.equalsIgnoreCase("id_persona")) {
+                    return getId_persona().compareTo(c.getId_persona()) < 0;
+                
+                }else if (field.equalsIgnoreCase("motivo")) {
+                    return getMotivo().compareTo(c.getMotivo()) < 0;
+                
+                } else if (field.equalsIgnoreCase("fechaDivorcio")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaDivorcio());
+                        Date otherDate = sdf.parse(c.getFechaDivorcio());
+                        return thisDate.compareTo(otherDate) < 0;
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }else if (field.equalsIgnoreCase("estadoCivilAnterior")) {
+                    return getEstadoCivilAnterior().compareTo(c.getEstadoCivilAnterior()) < 0;
+                
+                }else if (field.equalsIgnoreCase("estadoCivilActual")) {
+                    return getEstadoCivilActual().compareTo(c.getEstadoCivilActual()) < 0;
+                
+                }
+            default:
+                break;
+        }
+        return false;
+    }
+    
+    public Integer compareQuickSort(DetalleCenso c, Integer type, String field) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        switch (type) {
+        case 0:
+            if (field.equalsIgnoreCase("fechaCenso")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaCenso());
+                        Date otherDate = sdf.parse(c.getFechaCenso());
+                        return thisDate.compareTo(otherDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+            }else if (field.equalsIgnoreCase("id")) {
+                return this.getId().compareTo(c.getId());
+            }else if (field.equalsIgnoreCase("id_persona")) {
+                return this.getId_persona().compareTo(c.getId_persona());
+            }else if (field.equalsIgnoreCase("motivo")) {
+                return this.getMotivo().compareTo(c.getMotivo());
+            }else if (field.equalsIgnoreCase("fechaDivorcio")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaDivorcio());
+                        Date otherDate = sdf.parse(c.getFechaDivorcio());
+                        return thisDate.compareTo(otherDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+            }else if (field.equalsIgnoreCase("estadoCivilAnterior")) {
+                return this.getEstadoCivilAnterior().compareTo(c.getEstadoCivilAnterior());
+            }else if (field.equalsIgnoreCase("estadoCivilActual")) {
+                return this.getEstadoCivilActual().compareTo(c.getEstadoCivilActual());
+            }
+        case 1:
+            if (field.equalsIgnoreCase("fechaCenso")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaCenso());
+                        Date otherDate = sdf.parse(c.getFechaCenso());
+                        return otherDate.compareTo(thisDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+            }else if (field.equalsIgnoreCase("id")) {
+                return c.getId().compareTo(this.getId());
+            }else if (field.equalsIgnoreCase("id_persona")) {
+                return c.getId_persona().compareTo(this.getId_persona());
+            }else if (field.equalsIgnoreCase("motivo")) {
+                return c.getMotivo().compareTo(this.getMotivo());
+            }else if (field.equalsIgnoreCase("fechaDivorcio")) {
+                    try {
+                        Date thisDate = sdf.parse(this.getFechaDivorcio());
+                        Date otherDate = sdf.parse(c.getFechaDivorcio());
+                        return otherDate.compareTo(thisDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+            }else if (field.equalsIgnoreCase("estadoCivilAnterior")) {
+                return c.getEstadoCivilAnterior().compareTo(this.getEstadoCivilAnterior());
+            }else if (field.equalsIgnoreCase("estadoCivilActual")) {
+                return c.getEstadoCivilActual().compareTo(this.getEstadoCivilActual());
+            }
+        default:
+            throw new AssertionError();
+        }
+    }
+    
+     @Override
     public String toString(){
-        return fechaCenso+"--"+motivo+"--"+fechaDivorcio+"--"+estadoCivilAnterior+"--"+estadoCivilActual+"";
+        return fechaDivorcio;
     }
     
     
